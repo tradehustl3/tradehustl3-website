a943408908005de4f96e4e4b484dab63845cce3f
TRADE HUSTL3 <tradehustl3@gmail.com>
2026-08-23 17:15:28 -0400

    Connect Google Analytics 4

--- app/google-analytics.tsx
+++ app/google-analytics.tsx
@@ -2,9 +2,10 @@
 
 import Script from "next/script";
 
+const DEFAULT_MEASUREMENT_ID = "G-PHLN0C7BWF";
+
 export function GoogleAnalytics() {
-  const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  if (!measurementId) return null;
+  const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || DEFAULT_MEASUREMENT_ID;
 
   return (
     <>
@@ -12,6 +13,7 @@ export function GoogleAnalytics() {
       <Script id="ga4-init" strategy="afterInteractive">{`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
+        window.gtag = window.gtag || gtag;
         gtag('js', new Date());
         gtag('config', '${measurementId}', { page_path: window.location.pathname + window.location.search });
       `}</Script>
