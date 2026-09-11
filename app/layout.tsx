21cc9ba8adba21713dae791a4ef13d47fb48140e
TRADE HUSTL3 <tradehustl3@gmail.com>
2026-09-11 16:36:52 -0400

    Load mobile header spacing overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Anton, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import "./mobile-header-tune.css";
 import { GoogleAnalytics } from "./google-analytics";
 import { MetaCampaignViewTracker } from "./meta-view-content";
 import { SITE_DESCRIPTION, SITE_NAME, SITE_TITLE, SITE_URL } from "./site";
